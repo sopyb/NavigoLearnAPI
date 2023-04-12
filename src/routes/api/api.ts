e3f7a417d65b4d9a24bb3130f21326d642e1ec36
sopy <doimpt@sopy.one>
2023-04-12 09:31:05 +0300

    How long has this been like this

--- src/routes/api/api.ts
+++ src/routes/api/api.ts
@@ -3,13 +3,13 @@ import Paths from '@src/routes/constants/Paths';
 import AuthRouter from '@src/routes/api/Auth';
 import UsersRouter from '@src/routes/api/Users';
 
-const baseRouter = Router();
+const BaseRouter = Router();
 
 // Import all routes at base path
 const { Auth, Explore, Roadmaps, Users } = Paths;
 
 // Auth routes
-baseRouter.use(Auth.Base, AuthRouter);
+BaseRouter.use(Auth.Base, AuthRouter);
 
 // Explore routes
 // ...
@@ -18,6 +18,6 @@ baseRouter.use(Auth.Base, AuthRouter);
 // ...
 
 // Users routes
-baseRouter.use(Users.Base, UsersRouter);
+BaseRouter.use(Users.Base, UsersRouter);
 
-export default baseRouter;
+export default BaseRouter;
