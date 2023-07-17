110b2e53dcb736e267cd75edb1f1cba1742321a6
sopy <contact@sopy.one>
2023-07-17 17:42:33 +0300

    Rate limiting the ammoun tof auth calls you can make

--- spec/tests/authrouter.spec.ts
+++ spec/tests/authrouter.spec.ts
@@ -93,7 +93,7 @@ describe('Login Router', () => {
       .expect(HttpStatusCodes.FOUND)
       .expect(
         'Location',
-        new RegExp('^https:\\/\\/accounts.google.com\\/o\\/oauth2.+'),
+        new RegExp('^https:\\/\\/accounts\\.google\\.com\\/o\\/oauth2.+'),
       );
   });
 
--- src/routes/AuthRouter.ts
+++ src/routes/AuthRouter.ts
@@ -13,14 +13,36 @@ import {
   authRegister,
 } from '@src/controllers/authController';
 import validateBody from '@src/validators/validateBody';
+import { rateLimit } from 'express-rate-limit';
 
 const AuthRouter = Router();
+const LoginLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 10, // limit each IP to 100 requests per windowMs
+    message: 'Too many login attempts, please try again later.',
+  }),
+  RegisterLimiter = rateLimit({
+    windowMs: 360 * 1000, // 1 hour
+    max: 5, // limit each IP to 100 requests per windowMs
+    message: 'Too many register attempts, please try again later.',
+  }),
+  ResetPasswordLimiter = rateLimit({
+    windowMs: 360 * 1000, // 1 hour
+    max: 10, // limit each IP to 100 requests per windowMs
+    message: 'Too many reset password attempts, please try again later.',
+  });
 
-AuthRouter.post(Paths.Auth.Login, validateBody('email', 'password'), authLogin);
+AuthRouter.post(
+  Paths.Auth.Login,
+  LoginLimiter,
+  validateBody('email', 'password'),
+  authLogin,
+);
 
 AuthRouter.post(
   Paths.Auth.Register,
   validateBody('email', 'password'),
+  RegisterLimiter,
   authRegister,
 );
 
@@ -28,15 +50,20 @@ AuthRouter.post(
   Paths.Auth.ChangePassword,
   validateSession,
   validateBody('password', 'newPassword'),
+  ResetPasswordLimiter,
   authChangePassword,
 );
 
-AuthRouter.post(Paths.Auth.ForgotPassword, authForgotPassword);
+AuthRouter.post(
+  Paths.Auth.ForgotPassword,
+  ResetPasswordLimiter,
+  authForgotPassword,
+);
 
-AuthRouter.get(Paths.Auth.GoogleLogin, authGoogle);
+AuthRouter.get(Paths.Auth.GoogleLogin, LoginLimiter, authGoogle);
 AuthRouter.get(Paths.Auth.GoogleCallback, authGoogleCallback);
 
-AuthRouter.get(Paths.Auth.GithubLogin, authGitHub);
+AuthRouter.get(Paths.Auth.GithubLogin, LoginLimiter, authGitHub);
 AuthRouter.get(Paths.Auth.GithubCallback, authGitHubCallback);
 
 AuthRouter.delete(Paths.Auth.Logout, validateSession, authLogout);
