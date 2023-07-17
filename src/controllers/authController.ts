196b182f7229a62610cc7a8c928f8b952fac2787
sopy <contact@sopy.one>
2023-07-17 14:56:41 +0300

    Added password change and Google login functionalities to the authentication controller.
    The changes include adding authChangePassword, authForgotPassword, and authGoogleCallback functions to the authController. The implementation of Google login is done using Google OAuth2 authorization and user info endpoint responses. We also made changes to our AuthRouter to include routes for changing the password and the forgot password functionality.

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -7,6 +7,9 @@ import { comparePassword, saltPassword } from '@src/util/LoginUtil';
 import { createSaveSession } from '@src/util/sessionManager';
 import { UserInfo } from '@src/models/UserInfo';
 import { checkEmail } from '@src/util/EmailUtil';
+import EnvVars from '@src/constants/EnvVars';
+import axios from 'axios/index';
+import logger from 'jet-logger';
 
 /*
  * Interfaces
@@ -89,6 +92,46 @@ async function insertUserInfo(
   return (await db.insert('userInfo', userInfo)) >= 0;
 }
 
+async function updateUser(
+  db: DatabaseDriver,
+  userId: bigint,
+  user: User,
+  userInfo?: UserInfo,
+): Promise<boolean> {
+  if (userInfo) if (!(await updateUserInfo(db, userId, userInfo))) return false;
+
+  return await db.update('users', userId, user);
+}
+
+async function updateUserInfo(
+  db: DatabaseDriver,
+  userId: bigint,
+  userInfo: UserInfo,
+): Promise<boolean> {
+  return await db.update('userInfo', userId, userInfo);
+}
+
+async function getUserInfo(
+  db: DatabaseDriver,
+  userId: bigint,
+): Promise<UserInfo | undefined> {
+  return await db.get<UserInfo>('userInfo', userId);
+}
+
+async function getUser(
+  db: DatabaseDriver,
+  userId: bigint,
+): Promise<User | undefined> {
+  return await db.get<User>('users', userId);
+}
+
+async function getUserByEmail(
+  db: DatabaseDriver,
+  email: string,
+): Promise<User | undefined> {
+  return await db.getWhere<User>('users', 'email', email);
+}
+
 /*
  * Controllers
  */
@@ -98,13 +141,14 @@ export async function authLogin(
 ): Promise<unknown> {
   const { email, password } = req.body;
 
-  if (typeof password !== 'string') return _invalidLogin(res);
+  if (typeof email !== 'string' || typeof password !== 'string')
+    return _invalidLogin(res);
 
   // get database
   const db = new DatabaseDriver();
 
   // check if user exists
-  const user = await db.getWhere<User>('users', 'email', email);
+  const user = await getUserByEmail(db, email);
   if (!user) return _invalidLogin(res);
 
   // check if password is correct
@@ -112,7 +156,7 @@ export async function authLogin(
   if (!isCorrect) return _invalidLogin(res);
 
   // check userInfo table for user
-  const userInfo = await db.getWhere('userInfo', 'userId', user.id);
+  const userInfo = await getUserInfo(db, user.id);
   if (!userInfo)
     if (!(await insertUserInfo(db, user.id, new UserInfo(user.id))))
       return _serverError(res);
@@ -141,7 +185,7 @@ export async function authRegister(
   const db = new DatabaseDriver();
 
   // check if user exists
-  const user = await db.getWhere<User>('users', 'email', email);
+  const user = await getUserByEmail(db, email);
   if (!!user) return _conflict(res);
 
   if (!checkEmail(email) || password.length < 8) return _invalidBody(res);
@@ -155,4 +199,137 @@ export async function authRegister(
     return res
       .status(HttpStatusCode.Ok)
       .json({ message: 'Registration successful', success: true });
+}
+
+export async function authChangePassword(
+  req: RequestWithBody,
+  res: Response,
+): Promise<unknown> {
+  const { password, newPassword } = req.body;
+
+  if (typeof password !== 'string' || typeof newPassword !== 'string')
+    return _invalidBody(res);
+
+  // get database
+  const db = new DatabaseDriver();
+
+  // check if user is logged in
+  if (!req.session?.userId) return _serverError(res);
+  const userId = req.session.userId;
+
+  // check if user exists
+  const user = await getUser(db, userId);
+  if (!user) return _serverError(res);
+
+  // check if password is correct
+  const isCorrect = comparePassword(password, user.pwdHash || '');
+  if (!isCorrect) return _invalidLogin(res);
+
+  user.pwdHash = saltPassword(newPassword);
+
+  // update password in ussr
+  const result = await updateUser(db, userId, user);
+
+  if (result)
+    return res
+      .status(HttpStatusCode.Ok)
+      .json({ message: 'Password changed', success: true });
+  else return _serverError(res);
+}
+
+// eslint-disable-next-line @typescript-eslint/require-await
+export async function authForgotPassword(
+  req: RequestWithBody,
+  res: Response,
+): Promise<unknown> {
+  // TODO: implement
+  return res
+    .status(HttpStatusCode.NotImplemented)
+    .json({ error: 'Not implemented', success: false });
+}
+
+export function authGoogle(_: never, res: Response): unknown {
+  return res.redirect(
+    'https://accounts.google.com/o/oauth2/v2/auth?client_id=' +
+      EnvVars.Google.ClientID +
+      '&redirect_uri=' +
+      EnvVars.Google.RedirectUri +
+      '&response_type=code&scope=email%20profile',
+  );
+}
+
+export async function authGoogleCallback(
+  req: RequestWithBody,
+  res: Response,
+): Promise<unknown> {
+  const code = req.query.code;
+
+  if (typeof code !== 'string') return _invalidBody(res);
+
+  try {
+    // get access token
+    let response = await axios.post('https://oauth2.googleapis.com/token', {
+      client_id: EnvVars.Google.ClientID,
+      client_secret: EnvVars.Google.ClientSecret,
+      redirect_uri: EnvVars.Google.RedirectUri,
+      grant_type: 'authorization_code',
+      code,
+    });
+
+    // check if response is valid
+    if (response.status !== 200) return _serverError(res);
+    if (!response.data) return _serverError(res);
+
+    // get access token from response
+    const data = response.data as { access_token?: string };
+    const accessToken = data.access_token;
+    if (!accessToken) return _serverError(res);
+
+    // get user info
+    response = await axios.get(
+      'https://www.googleapis.com/oauth2/v2/userinfo',
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
+    const userData = response?.data as GoogleUserData;
+    if (!userData) return _serverError(res);
+
+    // get database
+    const db = new DatabaseDriver();
+
+    // check if user exists
+    let user = await getUserByEmail(db, userData.email);
+
+    if (!user) {
+      // create user
+      user = new User(userData.name, userData.email, 0, '');
+      user.googleId = userData.id;
+      user.id = await insertUser(db, userData.email, userData.name, '');
+    } else {
+      user.googleId = userData.id;
+
+      // update user
+      if (!(await updateUser(db, user.id, user))) return _serverError(res);
+
+      // check userInfo table for user
+      const userInfo = await getUserInfo(db, user.id);
+      if (!userInfo)
+        if (!(await insertUserInfo(db, user.id, new UserInfo(user.id))))
+          return _serverError(res);
+    }
+    // check if user was created
+    if (user.id === -1n) return _serverError(res);
+
+    // create session and save it
+    if (await createSaveSession(res, BigInt(user.id)))
+      return res
+        .status(HttpStatusCode.Ok)
+        .json({ message: 'Registration successful', success: true });
+  } catch (e) {
+    logger.err(e, true);
+    return _serverError(res);
+  }
 }
--- src/routes/AuthRouter.ts
+++ src/routes/AuthRouter.ts
@@ -1,38 +1,25 @@
-import { Response, Router } from 'express';
+import { Router } from 'express';
 import Paths from '@src/constants/Paths';
 import HttpStatusCodes from '@src/constants/HttpStatusCodes';
 import DatabaseDriver from '@src/util/DatabaseDriver';
-import { comparePassword, saltPassword } from '@src/util/LoginUtil';
-import User, { UserRoles } from '@src/models/User';
-import { randomBytes } from 'crypto';
+import User from '@src/models/User';
 import EnvVars from '@src/constants/EnvVars';
 import { NodeEnvs } from '@src/constants/misc';
-import axios, { AxiosError } from 'axios';
-import logger from 'jet-logger';
+import axios from 'axios';
 import { UserInfo } from '@src/models/UserInfo';
 import { RequestWithSession } from '@src/middleware/session';
-import { checkEmail } from '@src/util/EmailUtil';
 import validateSession from '@src/validators/validateSession';
-import { authLogin, authRegister } from '@src/controllers/authController';
+import {
+  authChangePassword,
+  authForgotPassword,
+  authGoogle,
+  authGoogleCallback,
+  authLogin,
+  authRegister,
+} from '@src/controllers/authController';
 import validateBody from '@src/validators/validateBody';
 
 const AuthRouter = Router();
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-function handleExternalAuthError(error, res: Response): void {
-  if (!(error instanceof Error)) return;
-  if (EnvVars.NodeEnv !== NodeEnvs.Test) logger.err(error);
-  if (error instanceof AxiosError) {
-    res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).json({
-      error: "Couldn't get access token from external service",
-    });
-  } else {
-    res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).json({
-      error: error.message || 'Internal server error',
-    });
-  }
-}
 
 AuthRouter.post(Paths.Auth.Login, validateBody('email', 'password'), authLogin);
 
@@ -42,195 +29,18 @@ AuthRouter.post(
   authRegister,
 );
 
-AuthRouter.use(Paths.Auth.ChangePassword, validateSession);
 AuthRouter.post(
   Paths.Auth.ChangePassword,
-  async (req: RequestWithSession, res) => {
-    // check if data is valid
-    const { password } = getInfoFromRequest(req);
-
-    // get new password from body
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const { newPassword } = req?.body || {};
-
-    // if not, return error
-    if (!password || !newPassword || !req.session?.userId) {
-      return res.status(HttpStatusCodes.BAD_REQUEST).json({
-        error: 'A valid session, the old and the new password are required',
-      });
-    }
-
-    // get database
-    const db = new DatabaseDriver();
-
-    // check if user exists
-    const user = await db.get<User>('users', req.session.userId);
-
-    // if not, return error
-    if (!user) {
-      return res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).json({
-        error: 'Invalid user',
-      });
-    }
-
-    // check if the password is correct
-    const isPasswordCorrect = comparePassword(password, user.pwdHash || '');
-
-    // if not, return error - if password is empty, allow change
-    if (!isPasswordCorrect && user.pwdHash !== '') {
-      return res.status(HttpStatusCodes.BAD_REQUEST).json({
-        error: 'Incorrect password',
-      });
-    }
-
-    // change password
-    const saltedPassword = saltPassword(newPassword as string);
-
-    // update user
-    const result = await db.update('users', req.session.userId, {
-      pwdHash: saltedPassword,
-    });
-
-    // check result
-    if (result) {
-      return res.status(HttpStatusCodes.OK).json({
-        message: 'Password changed successfully',
-      });
-    } else {
-      return res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).json({
-        error: 'Something went wrong',
-      });
-    }
-  },
+  validateSession,
+  validateBody('password', 'newPassword'),
+  authChangePassword,
 );
 
-AuthRouter.post(Paths.Auth.ForgotPassword, async (req, res) => {
-  // check if data is valid
-  const { email } = getInfoFromRequest(req);
+AuthRouter.post(Paths.Auth.ForgotPassword, authForgotPassword);
 
-  // if not, return error
-  if (!email) {
-    return res.status(HttpStatusCodes.BAD_REQUEST).json({
-      error: 'Email is required',
-    });
-  }
+AuthRouter.get(Paths.Auth.GoogleLogin, authGoogle);
+AuthRouter.get(Paths.Auth.GoogleCallback, authGoogleCallback);
 
-  // get database
-  const db = new DatabaseDriver();
-
-  // check if user exists
-  const user = await db.getWhere<User>('users', 'email', email);
-
-  // if not, return error
-  if (!user) {
-    return res.status(HttpStatusCodes.BAD_REQUEST).json({
-      error: 'User with this Email does not exist',
-    });
-  }
-
-  // TODO: send email with reset code
-  return res.status(HttpStatusCodes.NOT_IMPLEMENTED).json({
-    error: 'Not implemented',
-  });
-});
-
-AuthRouter.get(Paths.Auth.GoogleLogin, (req, res) => {
-  res.redirect(
-    'https://accounts.google.com/o/oauth2/v2/auth?client_id=' +
-      EnvVars.Google.ClientID +
-      '&redirect_uri=' +
-      EnvVars.Google.RedirectUri +
-      '&response_type=code&scope=email%20profile',
-  );
-});
-
-AuthRouter.get(Paths.Auth.GoogleCallback, async (req, res) => {
-  const code = req.query.code as string;
-
-  if (!code) {
-    return res.status(HttpStatusCodes.FORBIDDEN).json({
-      error: 'Error while logging in with Google',
-    });
-  }
-
-  try {
-    // get access token
-    let response = await axios.post('https://oauth2.googleapis.com/token', {
-      client_id: EnvVars.Google.ClientID,
-      client_secret: EnvVars.Google.ClientSecret,
-      redirect_uri: EnvVars.Google.RedirectUri,
-      grant_type: 'authorization_code',
-      code,
-    });
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    const accessToken = response?.data?.access_token as string;
-
-    // if no token, return error
-    if (!accessToken) throw new AxiosError('No access token received');
-
-    // get user data
-    response = await axios.get(
-      'https://www.googleapis.com/oauth2/v2/userinfo',
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      },
-    );
-    const userData = response?.data as GoogleUserData;
-
-    // if no userDisplay data, return error
-    if (!userData) throw new Error('No userDisplay data received');
-
-    // get database
-    const db = new DatabaseDriver();
-
-    // check if userDisplay exists
-    let user = await db.getWhere<User>('users', 'email', userData.email);
-
-    // if a userDisplay doesn't exist, create a new userDisplay
-    if (!user) {
-      const userId = await db.insert('users', {
-        email: userData.email,
-        name: userData.name,
-        googleId: userData.id,
-      });
-
-      user = await db.get<User>('users', userId);
-
-      if (!user) throw new Error('User not found');
-    } else if (!user.googleId) {
-      // if a userDisplay exists but doesn't have a Google id, add it
-      await db.update('users', user.id, {
-        googleId: userData.id,
-      });
-    }
-
-    //check if userDisplay has userInfo
-    const userInfo = await db.getWhere<UserInfo>('userInfo', 'userId', user.id);
-
-    // if not, create userInfo
-    if (!userInfo) {
-      const userInfoId = await db.insert('userInfo', {
-        userId: user.id,
-        profilePictureUrl: '',
-        bio: '',
-        quote: '',
-        blogUrl: '',
-        websiteUrl: '',
-        githubUrl: '',
-      });
-
-      // check if userInfo was created
-      if (userInfoId < 0) throw new Error('Could not create userInfo');
-    }
-
-    // save session
-    return await saveSession(res, user);
-  } catch (e) {
-    handleExternalAuthError(e, res);
-  }
-});
 AuthRouter.get(Paths.Auth.GithubLogin, (req, res) => {
   res.redirect(
     'https://github.com/login/oauth/authorize?client_id=' +
