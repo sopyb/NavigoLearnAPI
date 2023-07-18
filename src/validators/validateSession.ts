61ca011001a7c6f7445fcc54341a1e0d17aff805
sopy <contact@sopy.one>
2023-07-18 17:20:48 +0300

    Refactor session validation and add DB check
    Refactored the session validation function to improve readability by separating the invalid session response into its own function. Also, added a database check to ensure that the session token exists in the database, providing another layer of security. This change makes unauthorized access much less likely.

--- src/validators/validateSession.ts
+++ src/validators/validateSession.ts
@@ -1,19 +1,31 @@
 import { NextFunction, Response } from 'express';
 import HttpStatusCodes from '@src/constants/HttpStatusCodes';
 import { RequestWithSession } from '@src/middleware/session';
+import DatabaseDriver from '@src/util/DatabaseDriver';
 
-export default function (
+function invalidSession(res: Response): void {
+  res.status(HttpStatusCodes.UNAUTHORIZED).json({
+    error: 'Invalid session',
+    success: false,
+  });
+}
+
+export default async function (
   req: RequestWithSession,
   res: Response,
   next: NextFunction,
-): void {
+): Promise<void> {
   // if session isn't set, return forbidden
   if (!req.session) {
-    res
-      .status(HttpStatusCodes.UNAUTHORIZED)
-      .json({ error: 'Token not found, please login' });
-    return;
+    return invalidSession(res);
   }
+
+  // get db session
+  const db = new DatabaseDriver();
+
+  // count how maynt sessions there are with token
+  if ((await db.countWhere('sessions', 'token', req.session.token)) !== 1n)
+    return invalidSession(res);
 
   // call next()
   next();
