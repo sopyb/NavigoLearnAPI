b9e5c55b9892275cece91e204c5856678008bfc3
sopy <contact@sopy.one>
2023-09-06 21:05:02 +0300

    Add "Views" utility with roadmap view & impression functions
    Added a new "Views" utility in util folder which includes two new functions: "addRoadmapView" and "addRoadmapImpression". These functions assist in tracking views and impressions of each roadmap. This would allow for a more detailed statistical analysis of user interaction with the different roadmaps, fostering a better understanding of user behavior.

---
+++ src/util/Views.ts
@@ -0,0 +1,35 @@
+import DatabaseDriver, {
+  ResultSetHeader,
+} from '@src/util/Database/DatabaseDriver';
+import { RoadmapView } from '@src/types/models/RoadmapView';
+
+export async function addRoadmapView(
+  db: DatabaseDriver,
+  roadmapId: bigint,
+  userId?: bigint,
+): Promise<boolean> {
+  if (!userId) userId = -1n;
+  return (
+    (await db.insert(
+      'roadmapViews',
+      new RoadmapView({
+        userId,
+        roadmapId,
+        full: true,
+      }),
+    )) >= 0
+  );
+}
+
+export async function addRoadmapImpression(
+  db: DatabaseDriver,
+  roadmapId: bigint[],
+  userId?: bigint,
+): Promise<boolean> {
+  if (!userId) userId = -1n;
+  const values = roadmapId.map((id) => `(${id}, ${userId}, 0)`).join(', ');
+  return ((await db._query(`
+      INSERT INTO roadmapViews (roadmapId, userId, full)
+      VALUES ${values}
+  `)) as ResultSetHeader).affectedRows >= 0;
+}
